4cf2cb56dc18aa9115e0bf3c9ecac71959e4816e
ANIL_L <anilarangi6@gmail.com>
2025-12-29 19:20:02 +0530

    Made flag generator

---
+++ README.md
@@ -0,0 +1,9 @@
+# initialized new project
+
+# All country flag generator
+
+# Technology used - HTML, CSS, JS, Axios library
+
+API FOR Fetching country and falg - https://countriesnow.space/api/v0.1/countries/flag/images
+
+# created by ~ Mr. ANIL (Linkedin: @anilmonitorr | IG: @anilmonitor )
---
+++ app.js
@@ -0,0 +1,43 @@
+let apiUrl = "https://countriesnow.space/api/v0.1/countries/flag/images";
+let imgid = document.querySelector("#imgid");
+let country = document.querySelector("#country");
+
+async function getCountry() {
+  let Count = await axios(apiUrl);
+  let Country = Count.data.data;
+  for (let i = 0; i < Country.length; i++) {
+    let option = document.createElement("option");
+    country.appendChild(option);
+
+    option.innerText = Country[i].name;
+    option.value = Country[i].name;
+  }
+}
+
+getCountry();
+
+let btn = document.querySelector("button");
+btn.addEventListener("click", async () => {
+  let span = document.querySelector("span");
+  span.innerText = "";
+
+  if (country.value === "Select a country") {
+    console.log("Please select a country!!");
+
+    span.innerText = "Please select a country!!";
+  }
+  imgid.alt = "Loading flag...";
+
+  let apiurl = await axios(apiUrl);
+  let allObject = apiurl.data.data;
+
+  let findFlag = allObject.find((oneObj) => {
+    return oneObj.name === country.value;
+  });
+
+  if (findFlag) {
+    imgid.src = findFlag.flag;
+
+    imgid.style.height = "150px";
+  }
+});
